3e1de5bedcbc25d6f57a2556d4f6b3b53572019a
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-11-26 22:53:00 -0500

    Added HttpClient Service to make API calls.

---
+++ Services/TMDBClient.cs
@@ -0,0 +1,26 @@
+﻿using MovieSea.Models;
+using System.Net.Http.Json;
+
+namespace MovieSea.Services
+{
+	public class TMDBClient
+	{
+		private readonly HttpClient _httpClient;
+
+		public TMDBClient(HttpClient httpClient, IConfiguration config)
+		{
+			_httpClient = httpClient;
+
+			_httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+			_httpClient.DefaultRequestHeaders.Accept.Add(new("application/json"));
+
+			string apiKey = config["TMDBKey"] ?? throw new Exception("TMDBKey not found!");
+			_httpClient.DefaultRequestHeaders.Authorization = new("Bearer", apiKey);
+		}
+
+		public Task<PopularMoviePagedResponse?> GetPopularMoviesAsync()
+		{
+			return _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>("movie/popular");
+		}
+	}
+}
